928491a82fea3376d9620d03bdda27d7cf839cc9
NormalFall <Normal>
2024-03-27 21:49:28 -0400

    Fix pipewire module

--- Modules/Pipewire/default.nix
+++ Modules/Pipewire/default.nix
@@ -8,7 +8,7 @@ with lib; {
     enable = mkEnableOption "Enables Pipewire audio backend";
   };
 
-  config = mkIf {
+  config = mkIf cfg.enable {
     sound.enable = true;
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
