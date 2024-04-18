eba9b8fce8835e18a05ae696c8473c6c6a3c7ea6
NormalFall <loupid10@hotmail.com>
2024-04-17 20:13:19 -0400

    Add the hyprland ecosystem

--- HomeManagerModules/WindowManager/Hyprland/default.nix
+++ HomeManagerModules/WindowManager/Hyprland/default.nix
@@ -33,18 +33,44 @@ let
       description = "Basicly exec-once";
     };
 
-    theme = mkOption {
-      default = null;
-      description = "Hyprland Theme";
+    touchScreen = mkEnableOption "Enables or disables touchscreens";
+
+    idle = {
+      enable = mkEnableOption "Enables hypridle";
+
+      lockBeforeSleep = mkOption { default = true; };
+
+      dimTime = mkOption {
+        default = null;
+        description = "Dims every lights. Disabled if not set";
+      };
+
+      lockTime = mkOption {
+        default = 150;
+        description = "Time before hyprlock";
+      };
+      
+      sleepTime = mkOption {
+        default = 200;
+        description = "Time before sleep";
+      };
     };
 
-    touchScreen = mkEnableOption "Enables or disables touchscreens";
+    wallpaper = mkOption {
+      default = null;
+      description = "Wallpaper that hyprland uses";
+    };
+    
+    theme = mkOption {
+      default = null;
+      description = "Hyprland and hyprlock theme";
+    };
   };
 
-  theme = if !(builtins.isNull cfg.theme) then 
-    import (./Themes + "/${cfg.theme}.nix")
-  else
-    { general = {}; animations = {}; decoration = {}; };
+  theme = {
+    hyprland = { general = {}; animations = {}; decoration = {}; layerrule = {};};
+    hyprlock = {};
+  } // cfg.theme;
 in
   with lib; {
     
@@ -52,6 +78,9 @@ in
 
   imports = [ 
     inputs.hyprland.homeManagerModules.default
+    inputs.hyprpaper.homeManagerModules.default
+    inputs.hypridle.homeManagerModules.default
+    inputs.hyprlock.homeManagerModules.default
   ];
 
   config = mkIf cfg.enable {
@@ -63,6 +92,7 @@ in
         common.default = ["gtk" "hyprland"];
       };
     };
+
     home.pointerCursor = {
       gtk.enable = true;
       size = cfg.cursor.size;
@@ -72,6 +102,7 @@ in
 
     wayland.windowManager.hyprland = {
       enable = true;
+      systemd.variables = ["--all"];
       settings = {
         monitor = cfg.monitors;
 
@@ -89,13 +120,61 @@ in
 
         xwayland.force_zero_scaling = !cfg.fakeres;
 
-	general = ( theme.general // {
+	general = ( theme.hyprland.general // {
 	  sensitivity = 1;
         });
 
-        decoration = theme.decoration;
-        animations = theme.animations;
+        decoration = theme.hyprland.decoration;
+        animations = theme.hyprland.animations;
+        layerrule = theme.hyprland.layerrule;
       };
     };
+
+    services.hyprpaper = if cfg.wallpaper != null then {
+      enable = true;
+
+      wallpapers = [
+        ",${cfg.wallpaper}"
+      ];
+
+      preloads = [
+        "${cfg.wallpaper}"
+      ];
+    } else {};
+
+    services.hypridle = {
+      enable = cfg.idle.enable;
+      listeners = [
+        (if cfg.idle.dimTime != null 
+        then {
+          timeout = cfg.idle.dimTime;
+          onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl s 5% -s & ${pkgs.brightnessctl}/bin/brightnessctl -d *::kbd_backlight s 0 -s";
+          onResume = "${pkgs.brightnessctl}/bin/brightnessctl -r & ${pkgs.brightnessctl}/bin/brightnessctl -d *::kbd_backlight -r";
+        }
+        else {})
+
+        (if cfg.idle.lockTime != null 
+        then {
+          timeout = cfg.idle.sleepTime;
+          onTimeout = "loginctl lock-session";
+        }
+        else {})
+
+        (if cfg.idle.sleepTime != null 
+        then {
+          timeout = cfg.idle.sleepTime;
+          onTimeout = "systemctl suspend";
+        }
+        else {})
+      ];
+
+      
+      lockCmd = "pidof hyprlock || hyprlock";
+      beforeSleepCmd = if cfg.idle.lockBeforeSleep
+        then "loginctl lock-session"
+        else "";
+    };
+
+    programs.hyprlock = theme.hyprlock // {enable = true;};
   };
 }
