ccbf1a1e56c64b84fcd7264276f338c9287ea867
NormalFall <loupid10@hotmail.com>
2024-04-23 20:22:11 -0400

    Add a privacy

---
+++ Modules/Privacy/default.nix
@@ -0,0 +1,29 @@
+{lib, config, pkgs, ...}:
+let
+  cfg = config.privacy;
+in
+with lib; {
+  options.privacy = {
+    enable = mkEnableOption "Enable Common privacy tools";
+    mullvad = mkOption {
+      default = true;
+      description = "Enables mullcad common packages";
+    };
+
+    browsers = mkOption {
+      default = true;
+      description = "Enables Browsers";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.openvpn3.enable = true;
+    environment.systemPackages = with pkgs; [
+      wireguard-tools
+      v2raya
+    ]
+    ++ (if cfg.mullvad then [mullvad-closest] else [])
+    ++ (if cfg.browsers then [tor-browser] ++ (if cfg.mullvad then [mullvad-browser] else []) else []);
+    services.mullvad-vpn.enable = cfg.mullvad;
+  };
+}
