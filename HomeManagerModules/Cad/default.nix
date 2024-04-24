9e6cb320abd3672f0b613ae03b2048c15894fa3b
NormalFall <loupid10@hotmail.com>
2024-04-23 20:22:54 -0400

    Add a CAD module

---
+++ HomeManagerModules/Cad/default.nix
@@ -0,0 +1,21 @@
+{lib, config, pkgs, ...}:
+let
+  cfg = config.cad;
+in
+with lib; {
+  options.cad = {
+    enable = mkEnableOption "Enables rototyping, CAD tools and 3d printer software";
+    exclude = mkOption {
+      default = [];
+      description = "Exclude packages from being installed";
+    };
+  };
+
+  config = {
+    home.packages = lists.subtractLists cfg.exclude (with pkgs; [
+      kicad-unstable
+      fritzing
+      prusa-slicer
+    ]);
+  };
+}
