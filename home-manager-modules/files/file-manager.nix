2f7d60d2059c8535d69a18690d6b494f5aaa10ad
NormalFall <normfalldev@pm.me>
2024-05-14 10:31:35 -0400

    redo file management apps

---
+++ home-manager-modules/files/file-manager.nix
@@ -0,0 +1,13 @@
+{lib, config, pkgs, ...}:
+let
+  cfg = config.files.fileManager;
+in with lib; {
+  options.files.fileManager.enable = 
+    mkEnableOption "Enable nautilus as default filemanager";
+  
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.gnome.nautilus ];
+
+    xdg.mimeApps.defaultApplications."inode/directory" = "org.gnome.Nautilus.desktop";
+  };
+}
