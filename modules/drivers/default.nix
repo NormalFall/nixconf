00a3ca270995a6a4b148999d7fc372626e988d76
NormalFall <loupid10@hotmail.com>
2024-05-16 11:12:09 -0400

    Add nfts support

--- modules/drivers/default.nix
+++ modules/drivers/default.nix
@@ -4,9 +4,13 @@ let
 in
 with lib; {
   options.drivers = {
-    disks.enable = mkOption {
-      default = true;
-      description = "Enables common desktop services for disks";
+    disks = {
+      enable = mkOption {
+        default = true;
+        description = "Enables common desktop services for disks";
+      };
+
+      extraPartitionFormats = mkOption { default = true; };
     };
 
     logitech.enable = mkEnableOption "Enables drivers for logitechG and logitech mouses";
@@ -21,6 +25,7 @@ with lib; {
     services.gvfs.enable = cfg.disks.enable;
     services.devmon.enable = cfg.disks.enable;
     services.udisks2.enable = cfg.disks.enable;
+    boot.supportedFilesystems = mkIf cfg.disks.extraPartitionFormats [ "ntfs" ];
 
     services.upower.enable = cfg.laptop.enable;
     services.power-profiles-daemon.enable = cfg.laptop.enable;
