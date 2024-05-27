def71d67a7952a25222fdf93dd414f7a028cd7b6
NormalFall <normfalldev@pm.me>
2024-05-26 23:13:35 -0400

    replace retroarch with standalone emulator

--- home-manager-modules/gaming/default.nix
+++ home-manager-modules/gaming/default.nix
@@ -7,28 +7,27 @@ let
     mangohud
   ];
 
-  emulatorPkgs = {
-    cores = with pkgs.libretro; [
-      # Nintendo
-      melonds
-      bsnes
-      mgba
-      nestopia
-      parallel-n64
-      sameboy
-      dolphin
+  emulatorPkgs = with pkgs; {
+    # Nintendo
+    ds = melonDS;
+    snes = snes9x;
+    gba = mgba;
+    nes = nestopia-ue;
+    n64 = mupen64plus;
+    gb = sameboy;
+    wii = dolphin-emu;
+    wiiu = cemu;
+    switch = ryujinx;
 
-      # Sega
-      picodrive
-      genesis-plus-gx
-    ];
+    # Sega
+    sega = kega-fusion;
 
-    standalone = with pkgs; [
-      ryujinx
-    ];
+    # Sony
+    psp = ppsspp;
+    ps1 = pcsxr;
+    ps2 = pcsx2;
   };
-in
-with lib; {
+in with lib; {
   options.gaming = {
     enable = mkOption {
       default = true;
@@ -54,17 +53,10 @@ with lib; {
         default = true;
 	description = "Enables dolphin, ryujinx and retroarch with basic cores";
       };
-      
-      retroarch = {
-        enable = mkOption {
-	  default = true;
-	  description = "enables retroarch";
-	};
 
-        extraCores = mkOption {
-          default = [];
-	  description = "Add more cores for retroarch";
-	};
+      exclude = mkOption {
+        default = [];
+        description = "list of name of excluded consoles";
       };
     };
 
@@ -86,13 +78,12 @@ with lib; {
 
   config = mkIf cfg.enable {
     home.packages = 
-      (if cfg.emulation.enable then (emulatorPkgs.standalone 
-      ++ (if cfg.emulation.retroarch.enable then [ (pkgs.retroarch.override { cores = [] ++ (cfg.emulation.retroarch.extraCores ++ emulatorPkgs.cores); }) ] else [])) else [])
-      ++ (if cfg.osu then [ inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin ] else [])
+      (if cfg.osu then [ inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin ] else [])
       ++ (if cfg.minecraft then [ pkgs.prismlauncher ] else [])
       ++ (if cfg.lutris then [ pkgs.lutris ] else [])
       ++ (if cfg.steamExtra then steamExtraPkgs else [])
       ++ (if cfg.steam.enable then [ pkgs.steam ] else [])
-      ++ (if cfg.r2modman then [ pkgs.r2modman ] else []);
+      ++ (if cfg.r2modman then [ pkgs.r2modman ] else [])
+      ++ (if cfg.emulation.enable then (builtins.attrValues (builtins.removeAttrs emulatorPkgs cfg.emulation.exclude)) else []);
   };
 }
