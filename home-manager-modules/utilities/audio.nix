1ce6e34c74daef78b6c935038a23bdd2feb4aa29
NormalFall <93881091+NormalFall@users.noreply.github.com>
2024-08-12 23:27:13 -0400

    Create HM utilities module

---
+++ home-manager-modules/utilities/audio.nix
@@ -0,0 +1,7 @@
+{lib, config, pkgs, ...}:
+let
+  cfg = config.utilities.audio;
+in with lib; {
+  options.utilities.audio.enable = mkEnableOption "Enable common audio tools";
+  config.home.packages = mkIf cfg.enable [pkgs.pavucontrol pkgs.helvum];
+}
---
+++ home-manager-modules/utilities/disk.nix
@@ -0,0 +1,7 @@
+{lib, config, pkgs, ...}:
+let
+  cfg = config.utilities.disk;
+in with lib; {
+  options.utilities.disk.enable = mkEnableOption "Enable common disk management tools";
+  config.home.packages = mkIf cfg.enable [pkgs.gparted pkgs.popsicle];
+}
