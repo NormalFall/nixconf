61f116cf431ccf3baf05e5c6270077db47442137
NormalFall <93881091+NormalFall@users.noreply.github.com>
2024-08-22 20:59:13 -0400

    Add dev android module

--- home-manager-modules/dev/default.nix
+++ home-manager-modules/dev/default.nix
@@ -1,9 +1,10 @@
 { lib, config, ... }:
 with lib; {
-  imports = [ ./network-tools.nix ./vscodium.nix ];
+  imports = [ ./network-tools.nix ./vscodium.nix ./android.nix ];
 
   config = {
     dev.networkTools.enable = mkDefault true;
     dev.vscodium.enable = mkDefault true;
+    dev.android.enable = mkDefault true;
   };
 }
