606a7e0ffa42b68deded56950b8fdefbe9568f4b
NormalFall <normfalldev@pm.me>
2024-08-22 20:59:13 -0400

    Add dev android module

---
+++ home-manager-modules/dev/android.nix
@@ -0,0 +1,10 @@
+{lib, config, pkgs, ...}:
+let
+  cfg = config.dev.android;
+in with lib; {
+  options.dev.android.enable = mkEnableOption "Enable Android Studio";
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.android-studio ];
+  };
+}
