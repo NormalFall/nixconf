3ffd5aa5ac98ce9911dc09adda86ba036bcf81ef
NormalFall <normfalldev@pm.me>
2025-03-07 14:58:37 -0500

    Code-shell: fix bugs + change few behaviors + add basic logging

--- pkgs/code-shell/code-shell.sh
+++ pkgs/code-shell/code-shell.sh
@@ -1,3 +1,8 @@
+RED='\033[31m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+NC='\033[0m' # No Color
+
 nixshell_exists () {
     if [ -f $1/flake.nix ]; then
         printf "flake.nix"
@@ -11,6 +16,10 @@ nixshell_exists () {
 check_vscode () {
     command -v codium
     command -v code
+}
+
+open_dir () {
+    printf "SHELL="$SHELL" $CODE -n \"$DIR\""
 }
 
 usage () {
@@ -33,35 +42,53 @@ if [ -z "$CODE" ]; then
 fi
 
 # Find shell type
-if [ -z "$DIR" ]; then
-    SHELL_TYPE=$(nixshell_exists $CURRENT_DIR)
-    SHELL_PATH="$CURRENT_DIR/$SHELL_TYPE"
-elif [ -d $DIR ]; then
-    SHELL_TYPE=$(nixshell_exists $DIR)
-    SHELL_PATH="$DIR/$SHELL_TYPE"
-elif [ -f $DIR ]; then
-    if [ $DIR=*"/flake.nix" ]; then
+if [ -z "$DIR" ]; then # If no args are given look in current dir.
+    SHELL_TYPE=$(nixshell_exists "$CURRENT_DIR")
+    SHELL_PATH="$CURRENT_DIR"
+
+elif [ -d "$DIR" ]; then
+    SHELL_TYPE=$(nixshell_exists "$DIR")
+    SHELL_PATH="$DIR"
+
+elif [ -f "$DIR" ]; then
+    if [[ "$DIR" = */flake.nix ]]; then
         SHELL_TYPE="flake.nix"
-    elif [ $DIR=*"/shell.nix" ]; then
+    elif [[ "$DIR" = */shell.nix ]]; then
+        SHELL_TYPE="shell.nix"
+    elif [[ "$DIR" = *.nix ]]; then # It's possible to have shell files named other than shell.nix
         SHELL_TYPE="shell.nix"
     fi
-    DIR="$DIR/.."
-    SHELL_PATH="$DIR/$SHELL_TYPE"
+
+    if [ "$SHELL_TYPE" = "shell.nix" ]; then
+        SHELL_PATH="$DIR"
+        DIR="$DIR/.."
+    else
+        DIR="$DIR/.."
+        SHELL_PATH="$DIR"
+    fi
 else
-    printf "Invalid path!\n\n"
+    printf "${RED}Error${NC}: Invalid path!\n\n"
     usage
     exit 1
 fi
 
 # Check if shell type got found
 if [ -z "$SHELL_TYPE" ]; then
-    printf "Found no valid nix shell\n\n"
-    usage
-    exit 1
+    printf "${YELLOW}Warning${NC}: Found no valid nix shell. Opening VSCode anyway\n\n"
+    open_dir
+    exit 0
 fi
 
+printf "Shell type is: ${BLUE}${SHELL_TYPE}${NC}\n\n"
+
 if [ "$SHELL_TYPE" = "flake.nix" ]; then
-    nix develop $SHELL_PATH --command zsh -c "SHELL=zsh $CODE -n ."
+    nix develop "$SHELL_PATH" --command zsh -c "$(open_dir)"
+
+    # Run shell anyway if flake has no shell
+    if [ $? -ne 0 ]; then
+        printf "${YELLOW}Warning${NC}: Flake failed to get nix shell. Opening VSCode anyway\n\n"
+        open_dir
+    fi
 elif [ "$SHELL_TYPE" = "shell.nix" ]; then
-    nix-shell $SHELL_PATH --command "zsh -c \"SHELL=zsh $CODE -n .\""
+    nix-shell "$SHELL_PATH" --command "$(open_dir)"
 fi
