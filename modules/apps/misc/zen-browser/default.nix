130189e9897f50993615014dd9cc29a17cd761c2
NormalFall <93881091+NormalFall@users.noreply.github.com>
2025-04-02 20:24:40 -0400

    apps: zen-browser: set as default browser when enabled

--- modules/apps/misc/zen-browser/default.nix
+++ modules/apps/misc/zen-browser/default.nix
@@ -1,10 +1,25 @@
 {lib, pkgs, config, inputs, system, ...}:
 let
   cfg = config.misc.zen-browser;
+  browser-desktop-file = "zen.desktop";
 in with lib; {
   options.misc.zen-browser.enable = mkEnableOption "Enables zen-browser default pakcage";
 
-  config.environment.systemPackages = [
-    inputs.zen-browser.packages."${system}".default
-  ];
+  imports = [ ../../../core/main-user ];
+
+  config = {
+    environment.systemPackages = [
+      inputs.zen-browser.packages."${system}".default
+    ];
+
+    # Make sure to set it as default browser
+    mainUser.modules = [
+      ({...} : {
+        xdg.mimeApps.defaultApplications = {
+          "x-scheme-handler/http" = browser-desktop-file;
+          "x-scheme-handler/https" = browser-desktop-file;
+        };
+      })
+    ];
+  };
 }
