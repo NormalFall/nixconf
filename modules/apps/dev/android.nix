518399abe438420e52589fd61bb9fd45583dcf74
NormalFall <93881091+NormalFall@users.noreply.github.com>
2025-04-09 14:07:54 -0400

    dev: android: open react-native/expo port

--- modules/apps/dev/android.nix
+++ modules/apps/dev/android.nix
@@ -12,5 +12,8 @@ with lib;
   options.dev.android.enable = mkEnableOption "Enable Android Studio";
 
   # todo: add a check for if VM module is enabled
-  config = mkIf cfg.enable { environment.systemPackages = [ pkgs.android-studio ]; };
+  config = mkIf cfg.enable {
+    environment.systemPackages = [ pkgs.android-studio ];
+    networking.firewall.allowedTCPPorts = [ 8081 ]; # react native/expo ports
+  };
 }
