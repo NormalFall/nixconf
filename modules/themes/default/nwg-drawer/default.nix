ebf2d4dcd114093e74072d2d419c40cdf22aa000
NormalFall <93881091+NormalFall@users.noreply.github.com>
2025-10-15 13:52:23 -0400

    themes: default: change nwg for walker

--- modules/themes/default/nwg-drawer/default.nix
+++
@@ -1,5 +0,0 @@
-{ lib, config, ... }:
-{
-  imports = [ ../../../core/main-user ];
-  config.mainUser.modules = [({...} : {home.file.".config/nwg-drawer/drawer.css".source = ./nwg-drawer.css;})];
-}
