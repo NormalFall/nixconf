6ec715c61cba82c15e0715aab5a99e64e41302f8
NormalFall <normfalldev@pm.me>
2025-10-29 20:14:11 -0400

    theme: fix qt

--- modules/themes/default/qt.nix
+++ modules/themes/default/qt.nix
@@ -6,9 +6,7 @@
     ({...} : {
       qt = {
         enable = true;
-        platformTheme.name = "gtk";
         style.name = "adwaita-dark";
-        style.package = pkgs.adwaita-qt;
       };
     })
   ];
